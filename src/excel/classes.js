9d88cc51cef358f1139ed569f1b27b3bd8f08572
Mars7543 <miguelaroberts09@gmail.com>
2019-06-11 08:30:18 -0400

    updated excel parser

--- src/excel/classes.js
+++ src/excel/classes.js
@@ -1,32 +1,37 @@
-class Student{
+class Student {
     /**
-     * Constructor for the student object.
-     * Will be mainly used to create freshman objects.
-     * Take an academy, gender, name, address, town and zip.
-     * 
-     * THINGS TO DO:
-     * Name, when read fromt he file, looks like this:
-     *      "Kaluza, Alek"
-     * Figure out a way to separate this and make firstname and lastname variables.
-     */
-    constructor(academy, gender, name, address, town, zip){
-        this.academy = academy
-        this.gender = gender
-        tempnamearray = name.split(", ")
-        this.firstname = tempnamearray[1]
-        this.lastname = tempnamearray[0]
-        this.address = address
-        this.town = town
-        this.zip = zip
+        * Constructor for the student object.
+        * Will be mainly used to create freshman objects.
+        * Take an academy, gender, name, address, town and zip. 
+    **/
+    constructor(academy, gender, firstname, lastname, address, town, zip) {
+        this.academy    = academy
+        this.gender     = gender
+        this.firstname  = firstname
+        this.lastname   = lastname
+        this.address    = address
+        this.town       = town
+        this.zip        = zip
     }
-
-    changeAcademy(academy){
-        this.academy = academy
-    }
-
-    changeName
-    
 }
+
+class SparkStudent {
+    constructor(academy, firstname, lastname, town, knownStudents) {
+        this.academy    = academy
+        this.gender     = gender
+        this.firstname  = firstname
+        this.lastname   = lastname
+        this.town       = town
+    }
+}
+
+/**
+Constraints:
+   - Even # of males to females
+   - Good spread of academies (max 2 of one academy)
+   - Good spread of towns (including spark leader)
+   - Roughly same amnt of ppl per group (# of students / # of spark leaders) : 1 spark leader / group
+**/
 
 class Group {
     constructor() {
@@ -34,4 +39,4 @@ class Group {
     }
 }
 
-module.exports = { Student, Group }
+module.exports = { SparkStudent, Student, Group }
