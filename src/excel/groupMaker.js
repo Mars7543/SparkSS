a32be0f478fdfc22582f7cb565646db58558c05a
Mars7543 <miguelaroberts09@gmail.com>
2019-06-11 23:31:30 -0400

    added to groupmaker.js

--- src/excel/groupMaker.js
+++ src/excel/groupMaker.js
@@ -1,11 +1,34 @@
-const parse = require('./parse')
-const path  = require('path')
+const { getFreshmen, getSparkLeaders } = require('./parse')
 
-const freshmanList  = path.join(__dirname, '..', '..', 'public', 'assets', 'freshmanList.xlsx')
-const sparkList  = path.join(__dirname, '..', '..', 'public', 'assets', 'sparkLeaders.xlsx')
+const makeGroups = async () => {
+    try {
+        const freshmen = await getFreshmen()
+        const sparkLeaders = await getSparkLeaders()
 
-parse(freshmanList, 'Freshman')
-.then((freshmen) => {
-    console.log(freshmen)
-})
-.catch((e) => console.log(e))
+        let totalFreshman = 0
+        Object.values(freshmen).forEach((academy) => {
+            totalFreshman += academy.length
+        })
+
+        // a = # of freshman (288) b = # of spark leaders (33
+        // x = # of groups w 8 | y = # of groups w 9
+
+        // x + y = b
+        // 8x + 9y = 288
+
+        // 9x + 9y = 297
+        // 8x + 9y = 288
+        // x       = 9
+        // y       = 33 - x = 24
+
+        avg_group_size = Math.floor(totalFreshman / sparkLeaders.length)
+        group_of_ags = Math.floor(totalFreshman / avg_group_size)
+        group_of_ags_plus_1 = sparkLeaders.length - (totalFreshman % sparkLeaders.length)
+        console.log(avg_group_size, group_of_ags, group_of_ags_plus_1)
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+makeGroups()
--- src/excel/parse.js
+++ src/excel/parse.js
@@ -1,10 +1,13 @@
 const { Student, SparkStudent } = require('./classes')
 const readXlsxFile              = require('read-excel-file/node')
 
-module.exports = async (pathToFile, studentType) => {
+const path  = require('path')
+const freshmanList  = path.join(__dirname, '..', '..', 'public', 'assets', 'freshmanList.xlsx')
+const sparkList  = path.join(__dirname, '..', '..', 'public', 'assets', 'sparkLeaders.xlsx')
+
+module.exports.getFreshmen = async () => {
     try {
-        student_data = await readXlsxFile(pathToFile)
-        
+        student_data = await readXlsxFile(freshmanList)
         students = {
             'ATCS': [],
             'AMST': [],
@@ -18,38 +21,47 @@ module.exports = async (pathToFile, studentType) => {
         }
 
         student_data.forEach((student, index) => {            
-            if (index == 0 || index > 3) return // skip over headers row
+            if (index == 0) return // skip over headers row
 
-            if (studentType === 'Freshman') {
-                const [ academy, gender, name ] = student
-                const [ lastName, firstName ] = name.split(', ')
+            const [ academy, gender, name ] = student
+            const [ lastName, firstName ] = name.split(', ')
 
-                const address = student[8]
-                const town = student[9]
-                const zip = student[10]
+            const address = student[8]
+            const town = student[9]
+            const zip = student[10]
 
-                students[academy].push(new Student(academy, gender, firstName, lastName, address, town, zip))
-            } else if (studentType === 'Spark Leader') {
-                const [ academy, name, town, knownStudents ] = student
-                const [ lastName, firstName ] = name.split(', ')
+            students[academy].push(new Student(academy, gender, firstName, lastName, address, town, zip))
+        })
 
-                if (knownStudents) {
-                    tempStudents = []
-                    knownStudents = knownStudents.split(':')
-                    
-                    knownStudents.forEach((freshman) => {
-                        const [ freshman_lastName, freshman_firstName ] = freshman
-                    })
-                }
+        return students
+    } catch (e) {
+        throw new Error(e)
+    }
+}
 
-                // if (knownStudents)
-                //     knownFreshmen = knownStudents.split(':')
+module.exports.getSparkLeaders = async () => {
+    try {
+        student_data = await readXlsxFile(sparkList)
+        students = []
 
-                // if (typeof knownFreshmen !== 'undefined')
-                //     console.log(knownFreshmen)
+        student_data.forEach((student, index) => {            
+            if (index == 0) return // skip over headers row
 
-                students.push(new SparkStudent(academy, firstName, lastName, town, null))
+            const [ academy, name, town ]   = student
+            const [ lastName, firstName ]   = name.split(', ')
+            let knownStudents               = student[3]
+
+            if (knownStudents) {
+                tempStudents = []
+                knownStudents.split(':').forEach((freshman) => {
+                    const [ freshman_lastName, freshman_firstName ] = freshman.split(',')
+                    tempStudents.push(new Student(null, null, freshman_firstName, freshman_lastName, null, null, null))
+                })
+
+                knownStudents = tempStudents
             }
+
+            students.push(new SparkStudent(academy, firstName, lastName, town, knownStudents))
         })
 
         return students
